0f3a207c9da314928b9302c3e43226a898fd186f
KieranOdell <kfo7qb@virginia.edu>
2024-04-23 15:22:08 -0400

    trying again

---
+++ cville.py
@@ -0,0 +1,11 @@
+# This file is to model the bus system for Charlottesville, or more specifically UVA.
+# I am starting by just trying to set up the map.
+
+# Temp dummy weight
+weight = 0
+
+intersections = {
+    ("Main", "10th", "E"):[("Main", "JPA", "E", weight), ("JPA", "Main", "S", weight)],
+    ("Main", "JPA", "E"):[("Main", "14th", "E", weight), ("14th", "Main", "N", weight)],
+    ("Main", "14th", "E"):[("Main", "Rugby", "E", weight), ("Rugby", "Main", "N", weight)],
+    ("Main", "Rugby", "E"):[("Main", "Emmet", "E", weight), ("Emmet", "Main", "S", weight), ("Emmet", "Main", "N", weight)]}
